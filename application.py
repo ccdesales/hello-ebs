17af4419b514efafcaedd0fa6b07570d97e0d742
Cesar Desales <cesar.desales@webrepublic.ch>
2017-05-29 14:01:13 +0200

    Add EBExtensions and health check

--- application.py
+++ application.py
@@ -7,6 +7,11 @@ def hello_world():
     return 'Hello, World!'
 
 
+@application.route('/health', methods=['POST', 'GET'])
+def health():
+    return 'OK'
+
+
 if __name__ == "__main__":
     application.debug = True
     application.run()
